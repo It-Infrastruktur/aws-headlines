a7746eb78427132ddcfaf58da252f2d80e40d673
Felix Anhalt <felix.anhalt@camunda.com>
2019-11-15 11:32:30 +0100

    use correct format for api gateway

--- js/index.js
+++ js/index.js
@@ -2,7 +2,15 @@ const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('8566b11f65a14d54b8be47b1c01db39e');
 
 exports.handler = async (event) => {
+
   return newsapi.v2.topHeadlines({
     sources: `${event["queryStringParameters"]['sources']}`
+  }).then((resp) => {
+    const response = {"isBase64Encoded": false,
+                    "statusCode": 200,
+                    "headers": { "headerName": "headerValue" }
+                    };
+    response.body = JSON.stringify(resp);
+    return response;
   });
 };
