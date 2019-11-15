ff3ec504146be674b8cb2ba8de815701ea84353e
Felix Anhalt <felix.anhalt@camunda.com>
2019-11-15 11:12:27 +0100

    finish fun

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('8566b11f65a14d54b8be47b1c01db39e');
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-console.log('wow');
-exports.handler = newsapi.v2.topHeadlines({
-  sources: 'bbc-news,the-verge',
-  q: 'bitcoin',
-  category: 'business',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-    console.log(response);
-});
 
-console.log('done');
+exports.handler = async (event) => {
+  return newsapi.v2.topHeadlines({
+    sources: `${event["queryStringParameters"]['sources']}`
+  });
+};
